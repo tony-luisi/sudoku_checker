36f8241495feb05323f0eb748b7b081c971e8334
tony-luisi <t.luisi@gmail.com>
2016-05-20 12:09:18 +1200

    added row and col check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const board = [
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
+  [1,1,3,4,5,6,7,8,9],
+  [1,1,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
@@ -11,7 +11,8 @@ const board = [
 
 // console.log(createRandomSudoku())
 var sudokuBoard = createRandomSudoku()
-console.log(getBlockGroup(sudokuBoard, 4,4))
+// console.log(getBlockGroup(sudokuBoard, 3,3))
+checkSudoku(sudokuBoard)
 
 function createRandomSudoku(){
   var board = []
@@ -30,11 +31,52 @@ function createRandomSudoku(){
 }
 
 function checkSudoku(board){
+  var boardCorrect = true
+  for (var row = 1; row <= 9; row++){
+    if (!checkRow(board, row)){
+      boardCorrect = false
+    }
+  }
+  for (var col = 1; col <= 9; col++){
+    if (!checkCol(board, col)){
+      boardCorrect = false
+    }
+  }
+
+  console.log('board correct', boardCorrect)
   //check each row has 1 to 9 only once
-  
+
 
   //check each column has 1 to 9 only once
   //check each 3 by 3 grid has 1 to 9 only once
+}
+
+function checkRow(board, rowIndex){
+  var row = getRow(board, rowIndex)
+  var possibleValues = {}
+  var validRow = true
+  row.map(function(cell){
+    if (possibleValues[cell.value] === 'exists'){
+      validRow = false
+    } else {
+      possibleValues[cell.value] = 'exists'
+    }
+  })
+  return validRow
+}
+
+function checkCol(board, colIndex){
+  var col = getCol(board, colIndex)
+  var possibleValues = {}
+  var validCol = true
+  col.map(function(cell){
+    if (possibleValues[cell.value] === 'exists'){
+      validCol = false
+    } else {
+      possibleValues[cell.value] = 'exists'
+    }
+  })
+  return validCol
 }
 
 
@@ -42,12 +84,14 @@ function getRow(board, row){
  return board.filter(function(cell){ return cell.row === row })
 }
 
-function getColumn(board, col){
+function getCol(board, col){
   return board.filter(function(cell){ return cell.col === col })
 }
 
+//three block groups (1, 2, 3)
 function getBlockGroup(board, rowGroup, colGroup){
-
+  rowGroup = rowGroup * 3 - 2
+  colGroup = colGroup * 3 - 2
   return board.filter(function(cell){
     if (cell.row === rowGroup || cell.row === rowGroup+1 || cell.row === rowGroup + 2){
       if (cell.col === colGroup || cell.col === colGroup+1 || cell.col === colGroup+ 2){
